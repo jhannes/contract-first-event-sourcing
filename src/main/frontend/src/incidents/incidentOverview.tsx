538c4ab9208bc6e226a01154c47cd9c8c1c6be1b
Johannes Brodwall <johannes@brodwall.com>
2025-03-09 19:54:39 +0100

    move incidents to submodule

---
+++ src/main/frontend/src/incidents/incidentOverview.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+import { IncidentsSummaryList, IncidentSummary } from "./model";
+
+export function IncidentOverview() {
+  const [incidents, setIncidents] = useState<IncidentSummary[]>([]);
+  useEffect(() => {
+    const ws = new WebSocket("/ws");
+    ws.onmessage = (message) => {
+      const msg = JSON.parse(message.data) as IncidentsSummaryList;
+      console.log(msg);
+      setIncidents(msg.incidents);
+    };
+  }, []);
+  return (
+    <>
+      <h1>My incidents</h1>
+      {incidents.map(({ id, title }) => (
+        <div key={id} id={id}>
+          {title}
+        </div>
+      ))}
+      <h2>Create new incident</h2>
+      <form>
+        <div>
+          <label>
+            Title: <input />
+          </label>
+        </div>
+        <div>
+          <button>Submit</button>
+        </div>
+      </form>
+    </>
+  );
+}
--- src/main/frontend/src/main.tsx
+++ src/main/frontend/src/main.tsx
@@ -1,30 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
-import {
-  IncidentSummary,
-  IncidentsSummaryList,
-} from "../../../../target/generated-sources/openapi/typescript";
+import { IncidentOverview } from "./incidents/incidentOverview";
 
-function Application() {
-  const [incidents, setIncidents] = useState<IncidentSummary[]>([]);
-  useEffect(() => {
-    const ws = new WebSocket("/ws");
-    ws.onmessage = (message) => {
-      const msg = JSON.parse(message.data) as IncidentsSummaryList;
-      console.log(msg);
-      setIncidents(msg.incidents);
-    };
-  }, []);
-  return (
-    <>
-      <h1>My incidents</h1>
-      {incidents.map(({ id, title }) => (
-        <div key={id} id={id}>
-          {title}
-        </div>
-      ))}
-    </>
-  );
-}
-
-createRoot(document.getElementById("app")!).render(<Application />);
+createRoot(document.getElementById("app")!).render(<IncidentOverview />);
