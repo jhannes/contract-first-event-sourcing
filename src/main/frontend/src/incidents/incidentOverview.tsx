3efa02c406c4b4d59a4d888561411935063f0d83
Johannes Brodwall <johannes@brodwall.com>
2025-03-09 20:09:57 +0100

    client send new incident commands to server

--- src/main/frontend/src/incidents/incidentOverview.tsx
+++ src/main/frontend/src/incidents/incidentOverview.tsx
@@ -1,11 +1,24 @@
 import React, { useEffect, useState } from "react";
-import { IncidentsSummaryList, IncidentSummary } from "./model";
+import {
+  IncidentCommand,
+  IncidentsSummaryList,
+  IncidentSummary,
+} from "./model";
 import { NewIncidentForm } from "./newIncidentForm";
 
 export function IncidentOverview() {
   const [incidents, setIncidents] = useState<IncidentSummary[]>([]);
+  const [websocket, setWebsocket] = useState<WebSocket>();
+
+  function sendCommand(command: IncidentCommand) {
+    websocket?.send(JSON.stringify(command));
+  }
+
   useEffect(() => {
     const ws = new WebSocket("/ws");
+    ws.onopen = () => {
+      setWebsocket(ws);
+    };
     ws.onmessage = (message) => {
       const msg = JSON.parse(message.data) as IncidentsSummaryList;
       console.log(msg);
@@ -21,7 +34,7 @@ export function IncidentOverview() {
         </div>
       ))}
       <h2>Create new incident</h2>
-      <NewIncidentForm />
+      <NewIncidentForm sendCommand={sendCommand} />
     </>
   );
 }
--- src/main/frontend/src/incidents/newIncidentForm.tsx
+++ src/main/frontend/src/incidents/newIncidentForm.tsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { FormEvent, useState } from "react";
+import { IncidentCommand } from "./model";
+import { v4 as uuidv4 } from "uuid";
 
-export function NewIncidentForm() {
+export function NewIncidentForm({
+  sendCommand,
+}: {
+  sendCommand(command: IncidentCommand): void;
+}) {
+  const [title, setTitle] = useState("");
+  const [incidentId, setIncidentId] = useState(uuidv4());
+
+  function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    sendCommand({
+      incidentId,
+      delta: { title },
+    });
+    setIncidentId(uuidv4());
+    setTitle("");
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit} key={incidentId}>
       <div>
         <label>
-          Title: <input />
+          Title:{" "}
+          <input value={title} onChange={(e) => setTitle(e.target.value)} />
         </label>
       </div>
       <div>
