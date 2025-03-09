73cb83353e83d39ba47741a09710e2c97e12aa71
Johannes Brodwall <johannes@brodwall.com>
2025-03-09 20:41:05 +0100

    broadcast command to clients

--- src/main/frontend/src/incidents/incidentOverview.tsx
+++ src/main/frontend/src/incidents/incidentOverview.tsx
@@ -1,13 +1,12 @@
 import React, { useEffect, useState } from "react";
-import {
-  IncidentCommand,
-  IncidentsSummaryList,
-  IncidentSummary,
-} from "./model";
+import { IncidentCommand, IncidentSummary, MessageFromServer } from "./model";
 import { NewIncidentForm } from "./newIncidentForm";
 
-export function IncidentOverview() {
-  const [incidents, setIncidents] = useState<IncidentSummary[]>([]);
+function useApplicationWebSocket({
+  handleMessage,
+}: {
+  handleMessage: (message: MessageFromServer) => void;
+}) {
   const [websocket, setWebsocket] = useState<WebSocket>();
 
   function sendCommand(command: IncidentCommand) {
@@ -20,16 +19,36 @@ export function IncidentOverview() {
       setWebsocket(ws);
     };
     ws.onmessage = (message) => {
-      const msg = JSON.parse(message.data) as IncidentsSummaryList;
-      console.log(msg);
-      setIncidents(msg.incidents);
+      const msg = JSON.parse(message.data) as MessageFromServer;
+      handleMessage(msg);
     };
   }, []);
+
+  return { sendCommand };
+}
+
+export function IncidentOverview() {
+  const [incidents, setIncidents] = useState<IncidentSummary[]>([]);
+
+  function handleMessage(message: MessageFromServer) {
+    if ("incidents" in message) {
+      setIncidents(message.incidents);
+    } else if ("delta" in message) {
+      const {
+        delta: { title },
+        incidentId,
+      } = message;
+      setIncidents((old) => [...old, { title, incidentId }]);
+    }
+  }
+
+  const { sendCommand } = useApplicationWebSocket({ handleMessage });
+
   return (
     <>
       <h1>My incidents</h1>
-      {incidents.map(({ id, title }) => (
-        <div key={id} id={id}>
+      {incidents.map(({ incidentId, title }) => (
+        <div key={incidentId} id={incidentId}>
           {title}
         </div>
       ))}
--- src/main/java/com/johannesbrodwall/ApplicationWebSocketAdapter.java
+++ src/main/java/com/johannesbrodwall/ApplicationWebSocketAdapter.java
@@ -8,11 +8,20 @@ import org.eclipse.jetty.websocket.api.WebSocketAdapter;
 import org.eclipse.jetty.websocket.server.JettyServerUpgradeRequest;
 import org.eclipse.jetty.websocket.server.JettyServerUpgradeResponse;
 import org.eclipse.jetty.websocket.server.JettyWebSocketCreator;
-import org.openapitools.client.model.*;
+import org.openapitools.client.model.IncidentCommand;
+import org.openapitools.client.model.IncidentEvent;
+import org.openapitools.client.model.IncidentSummary;
+import org.openapitools.client.model.IncidentsSummaryList;
+import org.openapitools.client.model.MessageFromServer;
+import org.openapitools.client.model.MessageToServer;
+import org.openapitools.client.model.SampleModelData;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 import java.util.UUID;
 
 @Slf4j
@@ -22,6 +31,8 @@ public class ApplicationWebSocketAdapter implements JettyWebSocketCreator {
 
     private final Map<UUID, IncidentSummary> incidents = new HashMap<>();
 
+    private final Set<WebSocketAdapter> connectedClients = new HashSet<>();
+
     public ApplicationWebSocketAdapter() {
         var sampleData = new SampleModelData(-1);
         incidents.put(UUID.randomUUID(), sampleData.sampleIncidentSummary().setTitle("Fire"));
@@ -30,6 +41,24 @@ public class ApplicationWebSocketAdapter implements JettyWebSocketCreator {
 
     @Override
     public WebSocketAdapter createWebSocket(JettyServerUpgradeRequest req, JettyServerUpgradeResponse resp) {
+        var adapter = newWebSocketAdapter();
+        connectedClients.add(adapter);
+        return adapter;
+    }
+
+    @SneakyThrows
+    private void broadcastMessage(MessageFromServer messageToServer) {
+        var message = mapper.writeValueAsString(messageToServer);
+        for (var client : connectedClients) {
+            try {
+                client.getRemote().sendString(message);
+            } catch (IOException e) {
+                log.warn("Failed to send message to client {}", client, e);
+            }
+        }
+    }
+
+    private WebSocketAdapter newWebSocketAdapter() {
         return new WebSocketAdapter() {
             @SneakyThrows
             @Override
@@ -45,6 +74,9 @@ public class ApplicationWebSocketAdapter implements JettyWebSocketCreator {
             public void onWebSocketText(String message) {
                 var messageToServer = mapper.readValue(message, MessageToServer.class);
                 log.info(messageToServer.toString());
+                if (messageToServer instanceof IncidentCommand command) {
+                    broadcastMessage(new IncidentEvent().setTimestamp(System.currentTimeMillis()).putAll(command));
+                }
             }
 
             @Override
@@ -55,6 +87,7 @@ public class ApplicationWebSocketAdapter implements JettyWebSocketCreator {
             @Override
             public void onWebSocketClose(int statusCode, String reason) {
                 super.onWebSocketClose(statusCode, reason);
+                connectedClients.remove(this);
             }
         };
     }
