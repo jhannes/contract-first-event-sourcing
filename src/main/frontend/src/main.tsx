bdc84f8bcabfad50f3df25ccc797fd8a4c04cfe7
Johannes Brodwall <johannes@brodwall.com>
2025-03-09 19:51:41 +0100

    send messages from generated domain model

--- src/main/frontend/src/main.tsx
+++ src/main/frontend/src/main.tsx
@@ -1,14 +1,30 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { createRoot } from "react-dom/client";
+import {
+  IncidentSummary,
+  IncidentsSummaryList,
+} from "../../../../target/generated-sources/openapi/typescript";
 
 function Application() {
+  const [incidents, setIncidents] = useState<IncidentSummary[]>([]);
   useEffect(() => {
     const ws = new WebSocket("/ws");
     ws.onmessage = (message) => {
-      console.log("Received message", message);
+      const msg = JSON.parse(message.data) as IncidentsSummaryList;
+      console.log(msg);
+      setIncidents(msg.incidents);
     };
   }, []);
-  return <h1>My Nice App</h1>;
+  return (
+    <>
+      <h1>My incidents</h1>
+      {incidents.map(({ id, title }) => (
+        <div key={id} id={id}>
+          {title}
+        </div>
+      ))}
+    </>
+  );
 }
 
 createRoot(document.getElementById("app")!).render(<Application />);
--- src/main/java/com/johannesbrodwall/ApplicationWebSocketAdapter.java
+++ src/main/java/com/johannesbrodwall/ApplicationWebSocketAdapter.java
@@ -1,20 +1,41 @@
 package com.johannesbrodwall;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.SneakyThrows;
 import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.WebSocketAdapter;
 import org.eclipse.jetty.websocket.server.JettyServerUpgradeRequest;
 import org.eclipse.jetty.websocket.server.JettyServerUpgradeResponse;
 import org.eclipse.jetty.websocket.server.JettyWebSocketCreator;
+import org.openapitools.client.model.IncidentSummary;
+import org.openapitools.client.model.IncidentsSummaryList;
+import org.openapitools.client.model.SampleModelData;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 public class ApplicationWebSocketAdapter implements JettyWebSocketCreator {
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    private final Map<UUID, IncidentSummary> incidents = new HashMap<>();
+
+    public ApplicationWebSocketAdapter() {
+        var sampleData = new SampleModelData(-1);
+        incidents.put(UUID.randomUUID(), sampleData.sampleIncidentSummary().setTitle("Fire"));
+        incidents.put(UUID.randomUUID(), sampleData.sampleIncidentSummary().setTitle("Traffic accident"));
+    }
+
     @Override
     public WebSocketAdapter createWebSocket(JettyServerUpgradeRequest req, JettyServerUpgradeResponse resp) {
         return new WebSocketAdapter() {
             @SneakyThrows
             @Override
             public void onWebSocketConnect(Session sess) {
-                sess.getRemote().sendString("Hello world");
+                sess.getRemote().sendString(mapper.writeValueAsString(new IncidentsSummaryList().setIncidents(
+                        new ArrayList<>(incidents.values())
+                )));
             }
         };
     }
